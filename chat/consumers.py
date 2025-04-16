2d072dcfa96beaa63c9b111bd53eb866d3969e52
mrx <meibrohimov@gmail.com>
2025-04-16 14:38:24 +0500

    source file

---
+++ chat/consumers.py
@@ -0,0 +1,44 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = "public_chat"
+        self.room_group_name = "chat_%s" % self.room_name
+
+        # Guruhga ulanish
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Guruhdan chiqish
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data["message"]
+
+        # Guruhga xabar yuborish
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event["message"]
+
+        # Foydalanuvchiga xabar yuborish
+        await self.send(text_data=json.dumps({
+            "message": message
+        }))
---
+++ root/asgi.py
@@ -0,0 +1,13 @@
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(
+        chat.routing.websocket_urlpatterns
+    ),
+})
